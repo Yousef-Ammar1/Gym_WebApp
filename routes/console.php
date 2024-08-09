a5d5d972d99bcd8c18c04307e1c0d69de97256af
JChr15t <yousefaaaammar.com>
2024-08-09 08:58:59 -0400

    Scheduling the task

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,6 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
+Artisan::command('app:remind-members', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->dailyAt('12:58');
